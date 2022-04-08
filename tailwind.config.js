08faed0a863fbeeca928ab9f54b53553af02521b
Vert-Code <84845481+Vert-Code@users.noreply.github.com>
2022-04-08 11:04:46 +1000

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#597EE6',
+        secondary: '#F5F5F5',
+      }
+    },
+    fontFamil: {
+      sans: ['Red Hat Display', 'sans-serif'],
+    }
+  },
+  plugins: [],
+}
